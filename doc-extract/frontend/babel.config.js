e968185e9fdd4c352734d7c0de691650a15667ae
Raj Gogoi <gogoiraj221@gmail.com>
2026-09-19 20:55:55 +0530

    Fix Vercel frontend deployment

--- doc-extract/frontend/babel.config.js
+++ doc-extract/frontend/babel.config.js
@@ -2,7 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['@babel/preset-env', { targets: { node: 'current' } }],
+      ['@babel/preset-env', {}],
       '@babel/preset-react'
     ]
   };
